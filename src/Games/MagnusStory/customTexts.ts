ebdaa71d0792a2f0d18c383b8202ea6f2459e17b
Magnus Schurgers <magnus@strixit.com>
2019-03-03 14:55:43 +0100

    First checkin

---
+++ src/Games/MagnusStory/customTexts.ts
@@ -0,0 +1,9 @@
+namespace MagnusStory {
+    export class CustomTexts {
+        texts: StoryScript.IInterfaceTexts = {
+            // Add your custom texts here.
+            gameName: 'Magnus\' verhaal',
+            youAreHere: 'Hoofdstuk {0}'
+        }
+    }
+}
---
+++ src/Games/MagnusStory/run.ts
@@ -0,0 +1,4 @@
+namespace MagnusStory {
+    // Calling this function will bootstrap the game using our game namespace and rules and text objects.
+    StoryScript.Run('MagnusStory', new CustomTexts().texts);
+}
