73024466958a6e174d80b65c61da84ac6a031fb6
Rutger Schurgers <rutger@strixit.com>
2019-08-27 21:00:32 +0200

    Working on creating map function

--- src/Games/PathOfHeroes/maps.ts
+++ src/Games/PathOfHeroes/maps.ts
@@ -8,25 +8,62 @@ namespace PathOfHeroes {
 
     export function temparateFeatures() {
         if (!_temparateFeatures) {
-            _temparateFeatures = [
-                createTileFeature('top', 1, 2, Locations.Plains.name),
-                createTileFeature('topleft', 2, 1),
-                createTileFeature('topright', 2, 3),
-                createTileFeature('middle', 3, 2, Locations.Start.name),
-                createTileFeature('bottomleft', 4, 1, Locations.Water.name),
-                createTileFeature('bottom', 5, 2),
-                createTileFeature('bottomright', 4, 3)
+            var tileAdditions = [
+                ['12', Locations.Plains],
+                ['32', Locations.Start],
+                ['41', Locations.Water],
             ];
-
-            //_temparateFeatures.collectionHtml = '<visual-features img="tile demo.png"></visual-features>';
-            _temparateFeatures.collectionPicture = 'test';
+    
+            _temparateFeatures = createFeatureMap('tile demo.png', 'tm', 3, 3, 800, 600, TileType.Hexagon, tileAdditions);
+            // _temparateFeatures = [
+            //     createTileFeature('top', 1, 2, Locations.Plains.name),
+            //     createTileFeature('topleft', 2, 1),
+            //     createTileFeature('topright', 2, 3),
+            //     createTileFeature('middle', 3, 2, Locations.Start.name),
+            //     createTileFeature('bottomleft', 4, 1, Locations.Water.name),
+            //     createTileFeature('bottom', 5, 2),
+            //     createTileFeature('bottomright', 4, 3)
+            // ];
         }
 
         // Todo: this does not get reset on game restart. Use one of the setup rules?
         return _temparateFeatures;
     }
 
-    export function createTileFeature(name: string, row: number, column: number, linkToLocation?: string): IFeature {
+    export enum TileType {
+        Square,
+        Hexagon
+    }
+
+    export interface ITileFeature {
+        tile: string,
+        linkToLocation: () => ILocation
+    }
+
+    function createFeatureMap(mapPicture: string, prefix: string, rows: number, columns: number, tileWith: number, tileHeight: number, tileType: TileType, tileAdditions: (string | (() => ILocation))[][]): StoryScript.IFeatureCollection {
+        var map: StoryScript.IFeatureCollection = [];
+        map.collectionPicture = mapPicture;
+        
+        for (var i = 1; i <= rows; i++) {
+            for (var j = 1; j <= columns; j++) {
+                var featureName = `${prefix}${i}${j}`;
+                map.push(createTileFeature(featureName, i, j));
+            }
+        }
+
+        tileAdditions.forEach(a => {
+            var tile = <IFeature>map.filter(t => t.name === `${prefix}${a[0]}`)[0];
+
+            if (tile) {
+                var link = typeof a[1] === 'function' ? (<Function>a[1]).name : a[1].toString();
+                tile.linkToLocation = link;
+            }
+        });
+
+        return map;
+    }
+
+    function createTileFeature(name: string, row: number, column: number, linkToLocation?: string): IFeature {
         return StoryScript.DynamicEntity(() => Feature({
                 name: name,
                 shape: 'poly',
